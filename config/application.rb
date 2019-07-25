0c80e7546383c8d0e5eb09830adba56ca2dd77a9
MuneebDevs <muneebjavamob@gmail.com>
2019-07-25 15:39:58 +0500

    add patch and head

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module PosApi
           resource '*',
             headers: :any,
             expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-            methods: [:get, :post, :options, :delete, :put]
+            methods: [:get, :post, :delete, :put, :patch, :options, :head]
         end
       end
     end
