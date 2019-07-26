c84d439868f2e9986bacf9eafd4b110fa13d38fc
MuneebDevs <muneebjavamob@gmail.com>
2019-07-26 18:43:21 +0500

    implement relationship with invoices

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+	has_many :invoices, foreign_key: 'creator_id'
+
   # Include default devise modules. Others available are:
   #  :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
