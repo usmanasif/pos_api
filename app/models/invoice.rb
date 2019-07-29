299ae0fd19e485f110eacb4eac65159753be3987
MuneebDevs <muneebjavamob@gmail.com>
2019-07-29 17:12:39 +0500

    add accepts nested attributes for sold_items

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,11 @@
 class Invoice < ApplicationRecord
 	belongs_to :creator, class_name: "User"
 	has_many :sold_items
+
+	accepts_nested_attributes_for :sold_items
+	# , reject_if: :reject_posts
+
+  # def reject_posts(attributes)
+  #   attributes['title'].blank?
+  # end
 end
