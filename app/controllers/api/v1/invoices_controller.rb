cf1ba703c3db9ff6fe316a54bd69b18e0006a3cd
MuneebDevs <muneebjavamob@gmail.com>
2019-07-29 18:42:27 +0500

    remove comments

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -2,17 +2,14 @@ class Api::V1::InvoicesController < ApplicationController
   before_action :set_invoice, only: [:show]
   before_action :all_invoices, only:[:index]
   
-  # GET /invoices
   def index 
     render json: @invoices 
   end
 
-  # GET /invoices/1
   def show
     render json:(@invoice.attributes.merge("sold_items":@invoice.sold_items))
   end
 
-  # POST /invoices
   def create
     u = User.first
     @invoice = u.invoices.new(invoice_params)
@@ -25,7 +22,7 @@ class Api::V1::InvoicesController < ApplicationController
 
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_invoice
       @invoice = Invoice.find(params[:id])
     end
@@ -37,7 +34,6 @@ class Api::V1::InvoicesController < ApplicationController
       end
     end
 
-    # Only allow a trusted parameter "white list" through.
     def invoice_params
       params.require(:invoice).permit(:total, discount:[:discount,:discount_reason],adjustment:[:adjustment,:adjustment_reason] , sold_items_attributes: [:unit_price,:quantity,discount:[:discount,:discount_reason]])
     end
