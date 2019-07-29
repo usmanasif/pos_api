fa2d7108cc0d3796caf697dbf8f2185dd401f0cc
MuneebDevs <muneebjavamob@gmail.com>
2019-07-29 18:43:15 +0500

    remove comments

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,9 +3,5 @@ class Invoice < ApplicationRecord
 	has_many :sold_items
 
 	accepts_nested_attributes_for :sold_items
-	# , reject_if: :reject_posts
-
-  # def reject_posts(attributes)
-  #   attributes['title'].blank?
-  # end
+	
 end
