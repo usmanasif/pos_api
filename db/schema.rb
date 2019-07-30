55a893f608a4d87f2c437144e9dd7fb7f28d597a
Muneeb Ahmad <muneeb.ahmad@devsinc.com>
2019-07-30 15:18:54 +0500

    added new model for items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_111422) do
+ActiveRecord::Schema.define(version: 2019_07_30_075813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2019_07_26_111422) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "creator_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.integer "current_stock"
+    t.float "sale_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sold_items", force: :cascade do |t|
