6c02a71e7db2b4e84249c5ffc50e3af1a1386730
MuneebDevs <muneebjavamob@gmail.com>
2019-08-02 14:18:17 +0500

    add admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # devise_for :users
   mount_devise_token_auth_for 'User', at: 'auth'
+
+  mount_devise_token_auth_for 'Admin', at: 'admin_auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  namespace :api do
   namespace :v1 do
