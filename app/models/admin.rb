01dd23b6aeb5f7d7c2c24dddce1986e5be840484
MuneebDevs <muneebjavamob@gmail.com>
2019-08-02 15:31:08 +0500

    remove comments

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,5 @@
 class Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
     devise :database_authenticatable, :rememberable, :validatable
 
    include DeviseTokenAuth::Concerns::User
