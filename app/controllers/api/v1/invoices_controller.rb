f572ab5a97f90aa8396528b86b8455f15f68aabb
MuneebDevs <muneebjavamob@gmail.com>
2019-08-27 19:04:56 +0500

    enable auth

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::InvoicesController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_invoice, only: [:show]
 
   def index
