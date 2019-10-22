d382291aa1cea4c4ac48c991130626a3e4d07d51
salahuldin <salah.din@devsinc.com>
2019-10-22 17:20:54 +0500

    transaction filters

--- app/collections/transactions_collection.rb
+++ app/collections/transactions_collection.rb
@@ -14,5 +14,20 @@ class TransactionsCollection < BaseCollection
   end
 
   def ensure_filters
+    filter_transactions_by_vendor_name
+    filter_transactions_by_store_name
+    filter_transactions_by_date
+  end
+
+  def filter_transactions_by_vendor_name
+    filter {|relation| relation.where(vendor_id: Vendor.find_by_name(params[:vendor]).id)} if params[:vendor].present?
+  end
+
+  def filter_transactions_by_store_name
+    filter {|relation| relation.where(vendor_id: Vendor.find_by_store_name(params[:store]).id)} if params[:store].present?    
+  end
+
+  def filter_transactions_by_date
+    filter {|relation| relation.where("transaction_date between (?) and (?)",Date.parse(params[:startDate]).beginning_of_day,Date.parse(params[:endDate]).end_of_day)} if params[:startDate].present? && params[:endDate].present?
   end
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,5 +1,5 @@
 class Vendor < ApplicationRecord
   has_one :ledger
-  has_many :transections
+  has_many :transactions
   validates :name, :phone_number, :address, :code, :store_name, presence: true  
 end	
