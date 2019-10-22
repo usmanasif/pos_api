1367f5b5bcf7cd1c167626309df02ac776c77f2f
salahuldin <salah.din@devsinc.com>
2019-10-22 13:58:40 +0500

    vendor filter

--- app/collections/vendors_collection.rb
+++ app/collections/vendors_collection.rb
@@ -1,6 +1,6 @@
 class VendorsCollection < BaseCollection
   def meta
-    return { results: [ { total: total_count}, JSON.parse(results.to_json) ]}
+    return { results: [{total: total_count}, JSON.parse(results.to_json) ]}
   end
 
   private
@@ -14,5 +14,22 @@ class VendorsCollection < BaseCollection
   end
 
   def ensure_filters
+    filter_vendors_by_name
+    filter_vendors_by_store_name
+    filter_vendors_by_mobile_number
   end
+
+  def filter_vendors_by_name
+    filter {|relation| relation.where('name LIKE ?', '%' + params[:vendor] + '%')} if params[:vendor].present?
+  end
+
+  def filter_vendors_by_store_name
+    filter {|relation| relation.where('store_name LIKE ?', '%' + params[:store] + '%')} if params[:store].present?
+  end
+
+  def filter_vendors_by_mobile_number
+    filter {|relation| relation.where('phone_number LIKE ?', '%' + params[:mobile] + '%')} if params[:mobile].present?
+  end
+
+
 end
