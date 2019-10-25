c3af73f851f1b319eb39a1f22f98d69d5d982a4c
salahuldin <salah.din@devsinc.com>
2019-10-25 18:01:26 +0500

    customer transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :vendor, optional: true
+  belongs_to :customer, optional: true
   validates :amount, :transaction_date, :transaction_code, presence: true
   validates :transaction_code, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_110320) do
+ActiveRecord::Schema.define(version: 2019_10_25_125728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,14 @@ ActiveRecord::Schema.define(version: 2019_10_25_110320) do
     t.json "about_you"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.string "address"
+    t.string "code"
+    t.string "details"
   end
 
   create_table "discounts", force: :cascade do |t|
@@ -113,6 +121,7 @@ ActiveRecord::Schema.define(version: 2019_10_25_110320) do
     t.string "account_type"
     t.string "string"
     t.string "transaction_category"
+    t.string "customer_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -145,4 +154,3 @@ ActiveRecord::Schema.define(version: 2019_10_25_110320) do
   add_foreign_key "invoices", "users", column: "creator_id"
   add_foreign_key "sold_items", "invoices"
 end
-  
